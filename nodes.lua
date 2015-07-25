1e1eec85514ab8b36fb5625fbce683fa3c66523c
Veikko Mäkelä <veikk0@users.noreply.github.com>
2015-07-25 23:37:05 +0300

    Removed cobble nodes from the stone group

--- nodes.lua
+++ nodes.lua
@@ -3,55 +3,55 @@
 minetest.register_node("cobble_compression:compressed", {
 		description = "Compressed Cobblestone",
 		tiles = {"cc_node_01.png"},
-		groups = {cracky=3, stone=2},
+		groups = {cracky=3},
 		sounds = default.node_sound_stone_defaults(),
 })
 
 minetest.register_node("cobble_compression:doublecompressed", {
 		description = "Double Compressed Cobblestone",
 		tiles = {"cc_node_02.png"},
-		groups = {cracky=3, stone=2},
+		groups = {cracky=3},
 		sounds = default.node_sound_stone_defaults(),
 })
 
 minetest.register_node("cobble_compression:triplecompressed", {
 		description = "Triple Compressed Cobblestone",
 		tiles = {"cc_node_03.png"},
-		groups = {cracky=3, stone=2},
+		groups = {cracky=3},
 		sounds = default.node_sound_stone_defaults(),
 })
 
 minetest.register_node("cobble_compression:quadruplecompressed", {
 		description = "Quadruple Compressed Cobblestone",
 		tiles = {"cc_node_04.png"},
-		groups = {cracky=3, stone=2},
+		groups = {cracky=3},
 		sounds = default.node_sound_stone_defaults(),
 })
 
 minetest.register_node("cobble_compression:quintuplecompressed", {
 		description = "Quintuple Compressed Cobblestone",
 		tiles = {"cc_node_05.png"},
-		groups = {cracky=3, stone=2},
+		groups = {cracky=3},
 		sounds = default.node_sound_stone_defaults(),
 })
 
 minetest.register_node("cobble_compression:sextuplecompressed", {
 		description = "Sextuple Compressed Cobblestone",
 		tiles = {"cc_node_06.png"},
-		groups = {cracky=3, stone=2},
+		groups = {cracky=3},
 		sounds = default.node_sound_stone_defaults(),
 })
 
 minetest.register_node("cobble_compression:septuplecompressed", {
 		description = "Septuple Compressed Cobblestone",
 		tiles = {"cc_node_07.png"},
-		groups = {cracky=3, stone=2},
+		groups = {cracky=3},
 		sounds = default.node_sound_stone_defaults(),
 })
 
 minetest.register_node("cobble_compression:octuplecompressed", {
 		description = "Octuple Compressed Cobblestone",
 		tiles = {"cc_node_08.png"},
-		groups = {cracky=3, stone=2},
+		groups = {cracky=3},
 		sounds = default.node_sound_stone_defaults(),
 })
